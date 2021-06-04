6507551352478e627097897c2eb4b62f1a22bb87
John Bryan Valle <jbvalle.jv@gmail.com>
2021-06-04 10:30:30 +0200

    patch repair

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -7,21 +7,33 @@ public class Robot{
 
 		Robot_class r1 = new Robot_class("Zaptos", "blue", "Electricity");
 		Robot_class r2 = new Robot_class("Flami", "green", "Fire/Flight");
+		Robot_class r3 = new Robot_class("Pumeluf", "pink", "Confusion");
+		Robot_class r4 = new Robot_class("Glurak", "red", "Fire/Flight");
 		
 		r1.introduce();
 		r2.introduce();
+		r3.introduce();
+		r4.introduce();
 		
 		Person p1 = new Person("Said", 22);
 		Person p2 = new Person("Yasin", 23);
+		Person p3 = new Person("Jafer", 32);
+		Person p4 = new Person("Mushab", 23);
 		
 		p1.isOwned = r2;
 		p2.isOwned = r1;
+		p3.isOwned = r3;
+		p4.isOwned = r4;
 		
 		p1.sitdown();
 		p2.situp();
+		p3.sitdown();
+		p4.situp();
 		
 		p1.talk();
 		p2.talk();
+		p3.talk();
+		p4.talk();
 	}
 	
 }
